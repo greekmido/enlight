dc22c4356f0ff84d9652b00d7b118b7e964e39c2
mohammad alameri <onex911@hotmail.com>
2021-12-06 16:01:52 +0200

    this is first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Quotes from './Quotes';
+import React,{useState,useEffect, useCallback} from 'react';
 
 function App() {
+
+  const [quotesArr, setArray] = useState([]);
+  const [currentQuote, setCurrent] = useState(null);
+
+  useEffect(()=>{   
+    fetch('https://gist.githubusercontent.com/nasrulhazim/54b659e43b1035215cd0ba1d4577ee80/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
+    .then((response)=>response.json()).then(data=> setArray(data.quotes)).catch(e=>console.log(`Error fetching quotes${e}`));
+  },[]);
+
+  function getQuote(){
+        setCurrent(quotesArr[Math.floor(Math.random() * 101)])
+    }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+          {console.log(quotesArr)}
+          <Quotes />
+          <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
---
+++ src/Quotes.js
@@ -0,0 +1,13 @@
+
+
+function Quote(){
+     
+   return (
+       <blockquote>
+        this is mido
+       </blockquote>
+   )
+   }
+
+
+export default Quote;
