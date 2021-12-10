7dd6fdf757482341076a10e2acf7046389c54543
mohammad alameri <onex911@hotmail.com>
2021-12-10 16:25:27 +0200

    day16 finalizing for freecodecamp submit

--- src/Quotes.js
+++ src/Quotes.js
@@ -4,13 +4,14 @@ function Quotes(props){
   // const [currentQuote, setCurrent] = useState(props.qArr[Math.floor(Math.random() * 101)]);
    
   
-   return (<div>
-       <blockquote>
+   return (<div id="qoute-box">
+       <blockquote id="text">
         {props.randQuote.quote}
        </blockquote>
-      <p>
+      <p id="author">
           Author: {props.randQuote.author}
       </p>
+      <a href="twitter.com/intent/tweet">Tweet</a>
    </div>
    )
    }
